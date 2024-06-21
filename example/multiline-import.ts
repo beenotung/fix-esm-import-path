5e8113d80f422b1c64e4cb3fb64234279597591e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-06-21 09:16:19 +0800

    test: add test case for import/export type

--- example/multiline-import.ts
+++ example/multiline-import.ts
@@ -13,3 +13,19 @@ export {
   long_name_4,
   long_name_5,
 } from './multiline-export'
+
+export type {
+  long_type_1,
+  long_type_2,
+  long_type_3,
+  long_type_4,
+  long_type_5,
+} from './multiline-export'
+
+import type {
+  long_type_1,
+  long_type_2,
+  long_type_3,
+  long_type_4,
+  long_type_5,
+} from '.'
